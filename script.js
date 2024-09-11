c9e5d6f3afccb4c73238d45409098cf7e26a0671
xDreamBreaker <musor5@yandex.ru>
2024-09-11 14:59:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+document.getElementById('takeoff').addEventListener('click', function() {
+    console.log('Дрон взлетает');
+    // Здесь вы можете добавить вызовы к API или другие действия
+});
 
+document.getElementById('land').addEventListener('click', function() {
+    console.log('Дрон приземляется');
+});
+
+document.getElementById('move-forward').addEventListener('click', function() {
+    console.log('Дрон летит вперед');
+});
+
+document.getElementById('move-backward').addEventListener('click', function() {
+    console.log('Дрон летит назад');
+});
+
+document.getElementById('turn-left').addEventListener('click', function() {
+    console.log('Дрон поворачивает налево');
+});
+
+document.getElementById('turn-right').addEventListener('click', function() {
+    console.log('Дрон поворачивает направо');
+});
